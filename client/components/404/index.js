80b06d332f3c51031c598c9ff341b22e07d730aa
rifkegribenes <rifkegribenes@gmail.com>
2018-08-29 16:13:48 -0700

    style 404 page

--- client/components/404/index.js
+++ client/components/404/index.js
@@ -3,10 +3,9 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div>
-      <section className="content-header">
-        <h1>Page not found
-          <small>Please check the URL</small>
-        </h1>
+      <section className="content-header" style={{display: "flex", flexDirection: "column", justifyContent: "center", textAlign: "center", minHeight: "80vh"}}>
+        <h1>Page not found</h1>
+        <small>Please check the URL</small>
       </section>
     </div>
   );
